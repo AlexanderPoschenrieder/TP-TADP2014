8eef025cc291b6e4e0a344321c04e95563785df8
AlexanderPoschenrieder <aposchenrieder@yahoo.com.ar>
2014-05-07 01:01:42 -0300

    Refactor del ultimo test Agregue un test que verifique que se suman sin conflicto dos traits con el mismo nombre de metodo

--- TP-Traits/Test/alias_Trait_spec.rb
+++ TP-Traits/Test/alias_Trait_spec.rb
@@ -8,31 +8,35 @@ describe 'My behaviour' do
       :metodo1=> lambda{1},
       :metodo2=> lambda{2}
   }
+  bloques2= {
+      :metodo2=> lambda{3}
+  }
+
   Trait.define(:UnTrait,bloques1)
+  Trait.define(:OtroTrait,bloques2)
+  class Clase1
+    uses UnTrait<<(:metodo1>:nuevoMetodo1)
+  end
 
+  class Clase2
+    uses UnTrait + (OtroTrait<<(:metodo2>:metodo3))
+  end
+  unObjeto= Clase1.new()
+  unObjeto2= Clase2.new()
   it 'modificar el nombre de un metodo' do
-    class Clase1
-      uses UnTrait<<(:metodo1>:nuevoMetodo1)
-    end
 
-    unObjeto= Clase1.new()
     unObjeto.nuevoMetodo1.should==1
   end
 
   it 'deja de contener el metodo cambiado' do
-    class Clase2
-      uses UnTrait <<(:metodo1>:nuevoMetodo1)
-    end
-    unObjeto= Clase2.new()
     unObjeto.respond_to?(:metodo1).should==false
   end
 
   it 'no se modifica el trait original' do
-
-    class Clase3
-      uses UnTrait<< (:metodo1>:nuevoMetodo1)
-    end
-
     UnTrait.metodosTrait[:metodo1].should_not==nil
+  end
+  it 'suma de traits con mismos metodos y sin conflicto' do
+    unObjeto2.metodo2.should==2
+    unObjeto2.metodo3.should==3
   end
 end
