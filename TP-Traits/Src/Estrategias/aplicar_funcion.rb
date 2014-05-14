3ed4e677b3b69dff42e69de9a66ac4b4c6c241ef
AlexanderPoschenrieder <aposchenrieder@yahoo.com.ar>
2014-05-14 15:13:39 -0300

    Refactor en Estrategia

--- TP-Traits/Src/Estrategias/aplicar_funcion.rb
+++ TP-Traits/Src/Estrategias/aplicar_funcion.rb
@@ -1,4 +1,13 @@
 require 'TP-Traits/Src/Estrategias/estrategia'
 
 class Aplicar_funcion<Estrategia
+  attr_accessor :una_funcion
+  def initialize una_func
+    self.una_funcion =una_func
+  end
+
+  def armar_metodo unBloque,otroBloque
+    una_func(instance_eval(&unBloque),instance_eval(&otroBloque))
+  end
+
 end
--- TP-Traits/Test/usar_estrategia_spec.rb
+++ TP-Traits/Test/usar_estrategia_spec.rb
@@ -1,6 +1,5 @@
 require 'rspec'
 require  '../../TP-Traits/Src/Estrategias/ejecutar_en_orden'
-require '../../TP-Traits/Src/Estrategias/estrategia_chota'
 require '../Src/Trait/trait_suma'
 require '../../TP-Traits/Src/ClasesBase/clase'
 
@@ -16,10 +15,14 @@ describe 'My behaviour' do
   bloques3={
       :metodo1=> Proc.new{|unNumero| self.numero=unNumero}
   }
+  bloques4= {
+      :metodo1 => Proc.new{|unNumero| self.numero=self.numero+unNumero}
+  }
 
   Trait.define(:Trait1,bloques1)
   Trait.define(:Trait2,bloques2)
   Trait.define(:Trait3,bloques3)
+  Trait.define(:Trait4,bloques4)
 
   class Clase1
     attr_accessor :numero
@@ -28,6 +31,10 @@ describe 'My behaviour' do
   class Clase2
     attr_accessor :numero
   end
+  #class Clase4
+  #  attr_accessor :numero
+  #  uses (Trait3+Trait4).aplicar_estrategia(Ejecutar_en_orden.new)
+  #end
 
   it 'usa Estrategia ejecutar en orden' do
     unObjeto= Clase1.new
@@ -38,5 +45,11 @@ describe 'My behaviour' do
   it 'pincha si sumamos dos metodos con distinta aridad' do
     expect{Clase2.uses(Trait1+Trait3).aplicar_estrategia(Ejecutar_en_orden.new)}.to raise_error
   end
+
+  #it 'suma de dos metodos con varios parametros' do
+  #  unObjeto= Clase4.new
+  #  unObjeto.metodo1(4)
+  #  unObjeto.numero.should== 8
+  #end
 
 end
