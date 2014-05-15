eb136649ed504d2bb0ee92519116b2ecd15686c0
lautaro-garcia88 <lautaro.garcia88@gmail.com>
2014-05-14 22:51:06 -0300

    agregando * a lista de parametros en ejecutar en orden

--- TP-Traits/Src/Estrategias/ejecutar_en_orden.rb
+++ TP-Traits/Src/Estrategias/ejecutar_en_orden.rb
@@ -5,8 +5,8 @@ class Ejecutar_en_orden <Estrategia
   def armar_metodo unBloque, otroBloque
     lambda{
       |*parametros|
-      self.instance_exec parametros, &unBloque
-      self.instance_exec parametros, &otroBloque
+      self.instance_exec *parametros, &unBloque
+      self.instance_exec *parametros, &otroBloque
     }
   end
 end
