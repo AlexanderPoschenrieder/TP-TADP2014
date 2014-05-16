fa684d819ed565a220bfad8ae446bde3fefe9e7d
AlexanderPoschenrieder <aposchenrieder@yahoo.com.ar>
2014-05-16 18:43:00 -0300

    Arregle un problema en la estrategia a la hora de tirar excepcion por aridad

--- TP-Traits/Src/Estrategias/estrategia.rb
+++ TP-Traits/Src/Estrategias/estrategia.rb
@@ -1,18 +1,18 @@
 class Estrategia
   def  aplicar(un_trait)
     un_trait.conflictos.hashConflictos.each{ |nombreMetodo, arrayBloques|
-      self.checkear_aridad arrayBloques
+      self.checkear_aridad nombreMetodo, arrayBloques
       un_trait.metodos.store( nombreMetodo, armar_metodo(arrayBloques))
       un_trait.conflictos.remover(nombreMetodo)
     }
     un_trait
   end
 
-  def checkear_aridad arrayBloques
+  def checkear_aridad nombreMetodo, arrayBloques
     aridad = arrayBloques[0].arity
     arrayBloques.each{ |bloque|
        if ( bloque.arity != aridad )
-         raise("Existen dos métodos con el nombre:"+un_conflicto[0].to_s+" y con diferente aridad")
+         raise("Existen dos métodos con el nombre:"+ nombreMetodo.to_s+ " y con diferente aridad")
        end
     }
   end
